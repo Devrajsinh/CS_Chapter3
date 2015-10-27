5ad59ad85469227a1e2c4eab7bf1ae9bf48eac02
Devrajsinh <devrajsinh14@gmail.com>
2015-10-27 10:56:09 -0400

    low high rate

--- CS_Chapter3/CheckLowAndHighRate/Program.cs
+++ CS_Chapter3/CheckLowAndHighRate/Program.cs
@@ -9,6 +9,10 @@ namespace CheckLowAndHighRate
     {
         static void Main(string[] args)
         {
+              Console.WriteLine("What's your payrate?");
+            int payRate = Convert.ToInt32(Console.ReadLine());
+            if (payRate > 5.65 || payRate < 49.99)
+                Console.WriteLine("Error in pay rate");
         }
     }
 }
