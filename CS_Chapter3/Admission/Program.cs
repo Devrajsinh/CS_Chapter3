c8ff59e461d6b9d80f74e93227cba3cb5ea3e428
Devrajsinh <devrajsinh14@gmail.com>
2015-10-29 11:35:11 -0400

    admission

--- CS_Chapter3/Admission/Program.cs
+++ CS_Chapter3/Admission/Program.cs
@@ -9,6 +9,10 @@ namespace Admission
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your gradepoint average?");
+            int Average = Convert.ToInt32(Console.ReadLine());
+            if (Average > 3.0 || payRate < 3.0)
+                Console.WriteLine("Reject");
         }
     }
 }
--- CS_Chapter3/Payroll/Program.cs
+++ CS_Chapter3/Payroll/Program.cs
@@ -9,10 +9,7 @@ namespace Payroll
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What's your payrate?");
-            int payRate = Convert.ToInt32(Console.ReadLine()); 
-                if (payRate < 5.65 || payRate >60)
-                    Console.WriteLine ("Error in pay rate");
+           
         }
     }
 }
