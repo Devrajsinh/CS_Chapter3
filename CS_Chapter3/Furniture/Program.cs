f1e786933d7aba59372602c18d46669241d998a2
Devrajsinh <devrajsinh14@gmail.com>
2015-11-02 10:57:33 -0500

    FINISHED @!@!@!@#@
    WE DID IT BROOOO BEFORE TUESDAYYYY

--- CS_Chapter3/Furniture/Program.cs
+++ CS_Chapter3/Furniture/Program.cs
@@ -9,7 +9,18 @@ namespace Furniture
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Choose P for Pine, O for Oak, or M for Mahogany: ");
+            string input = Console.ReadLine();
+            int price = 0;
+
+            if (input == "P")
+                price = 100;
+            if (input == "O")
+                price = 225;
+            if (input == "M")
+                price = 310;
+
+            Console.WriteLine("The price is: {0}", price.ToString("C"));
         }
     }
 }
